2fea4e2bd94cf9e3836438a36302902e837b3a98
Eris-Kushikiari <erishallypena@gmail.com>
2025-08-05 21:13:06 +0800

    added a water effect

--- Chase Me/Assets/Script/CameraManager.cs
+++ Chase Me/Assets/Script/CameraManager.cs
@@ -11,7 +11,7 @@ public class CameraManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         FollowPlayer();
     }
--- Chase Me/Assets/Script/Player.cs
+++ Chase Me/Assets/Script/Player.cs
@@ -88,4 +88,5 @@ public class Player : MonoBehaviour
             playerRb.angularDamping = airAngularDrag;
         }
     }
+
 }
